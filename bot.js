8bcc86ac1f927c38c65e5f8e857b5227149fd8b8
vbriquet <vbriquet@gmail.com>
2018-03-30 11:58:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,6 +118,8 @@ sparkController.hears(['following-trains'], 'direct_message,direct_mention', dia
 });
 
 sparkController.hears(['smalltalk'], 'direct_message,direct_mention', dialogflowMiddleware.hears, function(bot, message) {
+    console.log ("JSON   message received from SmallTalk: " + JSON.stringify(message));
+    console.log ("Speech message received from SmallTalk: " + message.fulfillment.speech));
     bot.reply (message, message.fulfillment.speech);
 });
 
